2792d485c65afa5c304189043fbfd21b3aa94bb3
Will Emmanuel <will.emmanuel@gmail.com>
2015-04-06 12:23:54 -0400

    generate a new keypair with the app (also untested)

--- MultiSig/LoginViewController.m
+++ MultiSig/LoginViewController.m
@@ -10,15 +10,20 @@
 #import <coinbase-official/CoinbaseOAuth.h>
 #import "CoinbaseSingleton.h"
 
+#import <CoreBitcoin/BTCKey.h>
+
 @interface LoginViewController ()
 
 @end
 
-@implementation LoginViewController
+@implementation LoginViewController {
+    NSUserDefaults *_defaults;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [_loginButton addTarget:self action:@selector(didPressLogin) forControlEvents:UIControlEventTouchUpInside];
+    _defaults = [NSUserDefaults standardUserDefaults];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,20 +32,30 @@
 }
 -(void)didPressLogin {
     [_loginButton setTitle:@"Logging in..." forState:UIControlStateNormal];
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    if ([defaults objectForKey:@"access_token"] == nil) {
+    [_defaults synchronize];
+    if ([_defaults objectForKey:@"access_token"] == nil) {
         [CoinbaseOAuth startOAuthAuthenticationWithClientId:@"api_id"
                                                       scope:@"user balance"
                                                 redirectUri:@"edu.self.multisig.coinbase-oauth://coinbase-oauth"
                                                        meta:nil];
     } else {
-        Coinbase *apiClient = [Coinbase coinbaseWithOAuthAccessToken:[defaults objectForKey:@"access_token"]];
+        Coinbase *apiClient = [Coinbase coinbaseWithOAuthAccessToken:[_defaults objectForKey:@"access_token"]];
         [CoinbaseSingleton shared].client = apiClient;
         [self didFinishAuthentication];
     }
 }
 
 -(void) didFinishAuthentication {
+    if ([_defaults objectForKey:@"public_key"] == nil) {
+        BTCKey *newKey = [[BTCKey alloc] init];
+        // not very secure right now. fix later with keychain implementation
+        [_defaults setValue:[newKey privateKey] forKey:@"private_key"];
+        [_defaults setValue:[newKey publicKey] forKey:@"public_key"];
+        [_defaults synchronize];
+        newKey = nil;
+    }
+    // output public key (make sure it's there)
+    NSLog(@"%@", [_defaults objectForKey:@"public_key"]);
     [[CoinbaseSingleton shared].client doGet:@"accounts" parameters:nil completion:^(id result, NSError *error) {
         if (error) {
             NSLog(@"Could not load: %@", error);
